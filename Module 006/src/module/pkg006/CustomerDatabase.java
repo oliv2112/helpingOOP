7559bfd04fa812c94d8f0f967428a8eb923256de
Oliver Nordestgaard <42771071+olnor18@users.noreply.github.com>
2018-09-24 21:53:27 +0200

    Messed up commit-fix

--- Module 006/src/module/pkg006/CustomerDatabase.java
+++ Module 006/src/module/pkg006/CustomerDatabase.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -44,50 +43,3 @@ public class CustomerDatabase
         }
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package module.pkg006;
-
-/**
- *
- * @author Oliver
- */
-public class CustomerDatabase
-{
-    Customer[] customers = new Customer[999];
-
-    public CustomerDatabase()
-    {
-    }
-    
-    public void createCustomer(Customer c)
-    {
-        customers[c.id] = c;
-    }
-    
-    public void removeCustomer(int id)
-    {
-        customers[id] = null;
-    }
-    
-    public Customer getCustomer(int id)
-    {
-        return customers[id];
-    }
-    
-    public void printCustomers()
-    {
-        for(Customer c : customers)
-        {
-            if (c != null)
-            {
-                System.out.println(c.name+" with id: '"+c.id+"' has a balance of: $"+c.balance);
-            }
-        }
-    }
-}
->>>>>>> 9b72830ca118e8fc0624f19a0fd1c73d8ad84584
